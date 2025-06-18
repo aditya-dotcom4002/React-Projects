b0bd8b01413183a893d764ea226b2e379ebbfafc
aditya-dotcom <aceadityadto1234@gmail.com>
2025-06-18 14:42:10 +0530

    sixth commit

---
+++ 8thMiniContext/src/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const UserContext = React.createContext() //this is just a function like useState is a function... stored in a variable userContext
+
+export default UserContext;
